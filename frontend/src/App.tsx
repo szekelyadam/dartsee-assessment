8ed9242fddc59283c3f82c3893ea68d642209bfc
Ádám Székely <aszekelyadam@gmail.com>
2026-03-21 13:36:50 +0100

    feat: Integrate Tailwind CSS for styling

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,11 @@
 import "./App.css";
 
 function App() {
-  return <div>Dartsee</div>;
+  return (
+    <div className="text-4xl font-bold text-center text-blue-500 mt-10">
+      Dartsee
+    </div>
+  );
 }
 
 export default App;
